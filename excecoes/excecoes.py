9d5a9752efcaa2d27d1d73753cd9d281d5554f63
Ana Gomes Souza <202121190035@ifba.edu.br>
2025-10-17 21:48:40 -0300

    refactor: alteração de estrutura de pastas que resulta em melhor organização do código, delções geradas por essa refatoração

--- excecoes/excecoes.py
+++
@@ -1,7 +0,0 @@
-class EstaCheiaException(Exception):
-    """Exceção lançada quando a estrutura  está cheia."""
-    pass
-
-class EstaVaziaException(Exception):
-    """Exceção lançada quando a estrutura está vazia."""
-    pass
